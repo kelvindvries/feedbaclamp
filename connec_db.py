78007f13a3d6bbcc04a8fce87e42dabb00732532
Kelvin de Vries <Kelvin.devries@gmail.com>
2019-01-28 14:50:45 +0100

    Initial commit

---
+++ connec_db.py
@@ -0,0 +1,118 @@
+import psycopg2
+import random
+from config import config
+
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection configuration
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+def create_tables():
+    """ Create tables in the PostgreSQL database """
+
+    commands = (
+        """
+        CREATE TABLE feedbacklamp (
+            lamp_id SERIAL PRIMARY KEY,
+            lamp_decibel INTEGER
+        );
+        
+        DROP TABLE IF EXISTS feedbacklamp;
+        """)
+
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        # for command in commands:
+        cur.execute(commands)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def insert_feedbacklamp(lamp_decibel):
+    """ insert a new vendor into the vendors table """
+    sql = """INSERT INTO feedbacklamp(lamp_decibel)
+             VALUES(%s) RETURNING lamp_id;"""
+    conn = None
+    lamp_id = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (lamp_decibel,))
+        # get the generated id back
+        vendor_id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return lamp_id
+
+
+def get_db_value():
+    db_value = random.randint(10, 70)
+    return db_value
+
+
+def send_decibel(dBa_value, limit):
+    if dBa_value < limit:
+        print(f'NOPE: {dBa_value}')
+        send_decibel(get_db_value(), limit)
+    elif dBa_value >= limit:
+        print(f'YES: {dBa_value}')
+        return dBa_value
+
+
+# This Executes the program in order of functions
+if __name__ == '__main__':
+    send_decibel(get_db_value(), 50)
